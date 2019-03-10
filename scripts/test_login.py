46c65fa8d16e87bfa1885e52d07c5d8199a854ba
hitheima <hitheima@163.com>
2019-03-10 15:44:17 +0800

    登录数据的参数化

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,5 +1,7 @@
 import time
+import pytest
 
+from base.base_analyze import analyze_file
 from base.base_driver import init_driver
 from page.page import Page
 
@@ -14,10 +16,11 @@ class TestLogin:
         time.sleep(3)
         self.driver.quit()
 
-    def test_login(self):
+    @pytest.mark.parametrize(("phone", "password", "expect"), analyze_file("login_data", "test_login"))
+    def test_login(self, phone, password, expect):
         self.page.home.click_mine()
         self.page.mine.click_login_and_sign_up()
-        self.page.login_and_sign_up_page.input_phone("13800138006")
-        self.page.login_and_sign_up_page.input_password("123456")
+        self.page.login_and_sign_up_page.input_phone(phone)
+        self.page.login_and_sign_up_page.input_password(password)
         self.page.login_and_sign_up_page.click_login()
-        assert self.page.login_and_sign_up_page.is_login("登录成功")
+        assert self.page.login_and_sign_up_page.is_login(expect)
