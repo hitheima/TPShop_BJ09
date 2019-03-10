af2630000312698bfea4c8c242846cb214353576
hitheima <hitheima@163.com>
2019-03-10 10:13:54 +0800

    启动应用程序

--- base/base_driver.py
+++ base/base_driver.py
@@ -13,8 +13,8 @@ def init_driver():
     desired_caps['platformVersion'] = '5.1'
     desired_caps['deviceName'] = '192.168.56.101:5555'
     # app的信息
-    desired_caps['appPackage'] = 'com.android.contacts'
-    desired_caps['appActivity'] = '.activities.PeopleActivity'
+    desired_caps['appPackage'] = 'com.tpshop.malls'
+    desired_caps['appActivity'] = '.SPMainActivity'
     # 不要重置应用
     desired_caps['noReset'] = True
 
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,4 +1,16 @@
+import time
+
+from base.base_driver import init_driver
+
+
 class TestLogin:
+
+    def setup(self):
+        self.driver = init_driver()
+
+    def teardown(self):
+        time.sleep(1)
+        self.driver.quit()
 
     def test_login(self):
         print("1")
